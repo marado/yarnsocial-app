36fcee7e42572c637007447ba9b8a7df9ac7781a
eldersnake <eldersnake@noreply@mills.io>
2021-10-07 13:16:15 +0000

    Update 'scripts/setenv.sh' to coincide with android-build-wrapper.sh

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -17,13 +17,17 @@ fi
 {
   echo "storePassword=${ANDROID_KEY_STORE_PWD}"
   echo "keyPassword=${ANDROID_KEY_PWD}"
-  echo "keyAlias=key"
-  echo "storeFile=key.jks"
+  echo "keyAlias=upload"
+  echo "storeFile=upload-keystore.jks"
 } >> android/key.properties
 
-echo "${RELEASE_KEYSTORE}" > release.keystore.asc
-gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch release.keystore.asc > android/app/key.jks
+## Below has been commented out by eldersnake@yarn.andrewjvpowell.com
+## as we're generating the keystore in the android-build-wrapper.sh script
+## Uncomment out the below if running this script on its own (and if you know how to set the below variables!)
 
-mkdir -p keys/android
-echo "${RELEASE_SERVICE_ACCOUNT_KEYSTORE}" > service_account.keystore.asc
-gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch service_account.keystore.asc > keys/android/service_account.json
+#echo "${RELEASE_KEYSTORE}" > release.keystore.asc
+#gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch release.keystore.asc > android/app/key.jks
+
+#mkdir -p keys/android
+#echo "${RELEASE_SERVICE_ACCOUNT_KEYSTORE}" > service_account.keystore.asc
+#gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch service_account.keystore.asc > keys/android/service_account.json
